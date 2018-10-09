b296dcb5ac9f37a330d63a788038c9526103bb54
urmi-21 <mrbains21@gmail.com>
2018-10-09 16:03:09 -0500

    Update hw6/script.R

--- hw6/script.R
+++ hw6/script.R
@@ -9,4 +9,10 @@ ggplot(data=iowa,aes(x=as.factor(DRNKDRI2))) + geom_bar() + scale_y_log10()
 
 sort(table(iowa$DRNKDRI2,useNA = "ifany"))
 
+#iowa<-iowa %>% mutate(drinkdrive = (DRNKDRI2 == 88 )*0)
+iowa<-iowa %>% mutate(drinkdrive = ifelse(DRNKDRI2 == 88, 0, ifelse(DRNKDRI2 == 77 | DRNKDRI2 == 99, NA, DRNKDRI2)))
+
+
+iowa[,c('drinkdrive','DRNKDRI2')]
+
 #4
