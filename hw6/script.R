93b4c3451ec6dc9e8dc721d6dcebe6e4fde65501
urmi-21 <mrbains21@gmail.com>
2018-10-09 15:48:19 -0500

    Update hw6/script.R

--- hw6/script.R
+++ hw6/script.R
@@ -1,4 +1,12 @@
 setwd("~/GitHub/STAT579/hw6")
 
+#2
 iowa <- read.csv("iowa-brfss-2012.csv")
 library(tidyverse)
+
+#3
+ggplot(data=iowa,aes(x=as.factor(DRNKDRI2))) + geom_bar() + scale_y_log10()
+
+sort(table(iowa$DRNKDRI2,useNA = "ifany"))
+
+#4
