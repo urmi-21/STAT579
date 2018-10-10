31f6b1d8a1b708013712801c7e961e99ab2d502e
urmi-21 <mrbains21@gmail.com>
2018-10-09 23:57:03 -0500

    hw6

--- hw6/script.R
+++ hw6/script.R
@@ -13,6 +13,25 @@ sort(table(iowa$DRNKDRI2,useNA = "ifany"))
 iowa<-iowa %>% mutate(drinkdrive = ifelse(DRNKDRI2 == 88, 0, ifelse(DRNKDRI2 == 77 | DRNKDRI2 == 99, NA, DRNKDRI2)))
 
 
-iowa[,c('drinkdrive','DRNKDRI2')]
+iowa[which(iowa$DRNKDRI2==99),c('drinkdrive','DRNKDRI2')]
+
+summary(iowa[which(!is.na(iowa$drinkdrive)),c('drinkdrive')])
+length(iowa[which(!is.na(iowa$drinkdrive)),c('drinkdrive')])
+#for male
+males<-iowa[which(!is.na(iowa$drinkdrive) & iowa$SEX==1),c('drinkdrive')]
+summary(males)
+length(males)
+#female
+females<-iowa[which(!is.na(iowa$drinkdrive) & iowa$SEX==2),c('drinkdrive')]
+summary(females)
+length(females)
+
+t.test(males,females) 
+
 
 #4
+iowa$X_SMOKER3 <- as.factor(iowa$X_SMOKER3)
+head(iowa$X_SMOKER3)
+levels(iowa$X_SMOKER3)<-c("Current Smoker", "Current Smoker", "Former Smoker" ,"Never Smoked", "NA")
+head(iowa$X_SMOKER3)
+
