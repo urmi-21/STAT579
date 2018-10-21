881d000396c67e81de547ccbd9ffb2ca0ee161a5
urmi-21 <mrbains21@gmail.com>
2018-10-21 00:01:39 -0500

    hw7

--- hw7/script.R
+++ hw7/script.R
@@ -19,6 +19,35 @@ head(starwars)
 ChickWeight
 
 #2
-ChickWeight %>%  count(Chick) %>% filter(n <12)
-#or without dplyr
-table(ChickWeight$Chick)
+
+ChickWeight %>% count(Chick) %>% filter(n <12)
+
+complete<-ChickWeight %>% group_by(Chick) %>% mutate(count = n())  %>% filter(count==12)
+
+chkList<-unique(as.character(complete$Chick))
+
+#3
+complete<-complete  %>%  group_by(Chick) %>% mutate(weightgain = weight-first(weight))
+
+
+#4
+
+ggplot(data=complete %>% filter(Time==21),aes(x=Diet)) + geom_boxplot(aes(y=weightgain,fill=Diet))
+#sort increasing
+ggplot(data=complete %>% filter(Time==21),aes(x=reorder(Diet,weightgain,FUN = median))) + geom_boxplot(aes(y=weightgain,fill=reorder(Diet,weightgain,FUN = median)))+
+  xlab("Diet") + scale_fill_discrete(guide = guide_legend(title = "Diet"))
+
+#5
+
+ggplot(data=complete ,aes(x=Time,y=weight))+ geom_point(aes(colour=Diet)) + facet_wrap(~Diet)+ geom_line(aes(x=Time,y=weight, group = Chick, colour=Diet))
+
+#6
+
+
+ggplot(data=complete%>%group_by(Diet)%>%filter(Time==21)%>%filter(weight==max(weight)) ,aes(x=Time,y=weight))+ geom_point(aes(colour=Diet)) 
+
+complete %>% group_by(Time,Diet) %>% mutate(a=ave(weight))
+
+ggplot(data=complete %>% group_by(Time,Diet) %>% mutate(avgWt=ave(weight)) ,aes(x=Time,y=avgWt))+ geom_point(aes(colour=Diet)) + facet_wrap(~Diet)+ geom_line(aes(x=Time,y=avgWt, colour=Diet))
+
+
