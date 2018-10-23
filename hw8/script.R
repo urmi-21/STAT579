4c85dc37c4da2abcaef0a09042a7147318132402
urmi-21 <mrbains21@gmail.com>
2018-10-23 13:43:32 -0500

    hw8

--- hw8/script.R
+++ hw8/script.R
@@ -1,5 +1,5 @@
 setwd("~/GitHub/STAT579/hw8")
-library(dplyr)
+library(tidyverse)
 library(ggplot2)
 library(readr)
 starwars <- read_csv("StarWars.csv")
@@ -20,4 +20,34 @@ dim(starwars)[1]
 starwars_yes<-starwars[which(starwars$`Have you seen any of the 6 films in the Star Wars franchise? Response` == 'Yes'),]
 #people watched sw
 dim(starwars_yes)[1]
+
+
+#4
+starwars_yes %>% count(`Gender Response`)
+starwars_yes %>% count(`Age Response`)
+#make factor and arrange levels
+starwars_yes$`Age Response`<-factor(starwars_yes$`Age Response`, levels=c("18-29", "30-44", "45-60", "> 60", ""))
+levels(starwars_yes$`Age Response`)
+
+#5
+starwars_yes[, 10:15]<-sapply(starwars_yes[, 10:15], as.numeric)
+
+xY<-starwars_yes %>% gather(Ep,Rating,colnames(starwars_yes)[10:15])
+
+xY<-xY[which(!is.na(xY$Rating)),] %>% group_by(Ep) %>% mutate(m=mean(Rating))
+
+#MEAN BY MALE AND FEMALE  
+meanRatings<-xY[which(!is.na(xY$Rating)),] %>% group_by(Ep,`Gender Response`) %>% summarise_at(vars("Rating"), funs(mean, length))
+
+ggplot(data=meanRatings,aes(x=`Gender Response`)) + geom_boxplot(aes(y=mean))
+
+
+#6
+
+popularity<-starwars_yes[,25:26] %>% gather(Character,pop,colnames(starwars_yes)[25:26])
+#remove NA Unfamiliar (N/A)
+popularity<-popularity %>% filter(!is.na(pop))%>% filter(!(pop == "Unfamiliar (N/A)"))
+#map popularity to scale -2 -1 0 1 2
+# very unfavorably Somewhat unfavorably neutral Somewhat favorably Very favorably
+unique(popularity$pop)
 
