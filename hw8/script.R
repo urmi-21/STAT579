2116f5b7a74d6e2a4f4c5974550670eb7e9fe544
urmi-21 <mrbains21@gmail.com>
2018-10-23 12:26:14 -0500

    hw7

--- hw8/script.R
+++ hw8/script.R
@@ -13,3 +13,11 @@ names(starwars) <- enc2native(stringi::stri_trans_general(varnames, "latin-ascii
 
 starwars <- starwars[-1,]
 head(starwars)
+
+#3
+#how many responded
+dim(starwars)[1]
+starwars_yes<-starwars[which(starwars$`Have you seen any of the 6 films in the Star Wars franchise? Response` == 'Yes'),]
+#people watched sw
+dim(starwars_yes)[1]
+
