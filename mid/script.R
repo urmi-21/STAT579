27fedd396bc592e0bbcecb5a83a5cd3870f20036
urmi-21 <mrbains21@gmail.com>
2018-10-30 13:15:14 -0500

    s

--- mid/script.R
+++ mid/script.R
@@ -6,7 +6,7 @@ gss <- readRDS("gss.rds")
 #num rows
 dim(gss)[1]
 #num inter by years
-t<- gss %>% count(YEAR)
+gss %>% count(YEAR)
 #num vars
 dim(gss)[2]
 #most interviews
@@ -15,7 +15,7 @@ gss %>% count(YEAR) %>% filter(n==max(n))
 ggplot(data=gss %>% count(YEAR), aes(x=YEAR,y=n))+geom_bar(stat="identity",aes(fill=YEAR))
 
 
-#3
+#2
 
 levels(gss$AGE)[which(levels(gss$AGE)=='89 OR OLDER')]=89
 levels(gss$AGE)[which(levels(gss$AGE)=='DK')]=NA
@@ -27,6 +27,33 @@ gss1 <-  gss1 %>% mutate(AGE_num = replace(AGE_num, AGE_num=="NA", NA))
 sum(is.na(gss1$AGE_num))
 
 ggplot(data=gss1,aes(x=AGE_num))+geom_histogram(stat="count")+facet_wrap(~SEX,ncol = 1)+theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+
+#3
+
+#freq of degree
+gss1 %>% count(DEGREE)
+#remove DK, IAP
+levels(gss1$DEGREE)[which(levels(gss1$DEGREE)=='DK')]=NA
+levels(gss1$DEGREE)[which(levels(gss1$DEGREE)=='IAP')]=NA
+
+levels(gss1$DEGREE)<-c('LT HIGH SCHOOL', 'HIGH SCHOOL', 'JUNIOR COLLEGE', 'BACHELOR', 'GRADUATE', "NA")
+gss1 <-  gss1 %>% mutate(DEGREE = replace(DEGREE, DEGREE=="NA", NA))
+
+ggplot(data=gss1%>%filter(!is.na(DEGREE)),aes(x=AGE_num))+geom_histogram(stat="count")+facet_wrap(~DEGREE,ncol = 1)+theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+#4
+
+gss1 %>% group_by(SEX) %>% filter(!is.na(AGEKDBRN)) %>% summarize(avg=mean(AGEKDBRN),sd=sd(AGEKDBRN),obs=n())
+
+avgs <- gss1 %>% group_by(YEAR,SEX,DEGREE) %>% filter(!is.na(AGEKDBRN)) %>% summarize(avg=mean(AGEKDBRN),sd=sd(AGEKDBRN),obs=n()) %>% filter(obs>=30)
+
+ggplot(data=avgs, aes(x=avg))+geom_histogram() + facet_wrap(SEX~DEGREE)
+
+
+
 
 
 
