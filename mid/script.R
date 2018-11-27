122d81d75208b43d7999d4a0954a3fb8c3aee548
urmi-21 <mrbains21@gmail.com>
2018-11-27 16:26:02 -0600

    update mid

--- mid/script.R
+++ mid/script.R
@@ -12,7 +12,7 @@ dim(gss)[2]
 #most interviews
 gss %>% count(YEAR) %>% filter(n==max(n))
 #plot by year
-ggplot(data=gss %>% count(YEAR), aes(x=YEAR,y=n))+geom_bar(stat="identity",aes(fill=YEAR))
+ggplot(data=gss %>% count(YEAR), aes(x=YEAR,y=n))+geom_bar(stat="identity",aes(fill=factor(YEAR)))
 
 
 #2
@@ -38,7 +38,9 @@ gss1 %>% count(DEGREE)
 levels(gss1$DEGREE)[which(levels(gss1$DEGREE)=='DK')]=NA
 levels(gss1$DEGREE)[which(levels(gss1$DEGREE)=='IAP')]=NA
 
-levels(gss1$DEGREE)<-c('LT HIGH SCHOOL', 'HIGH SCHOOL', 'JUNIOR COLLEGE', 'BACHELOR', 'GRADUATE', "NA")
+#reorder levels
+gss1$DEGREE <- factor(gss1$DEGREE,levels(gss1$DEGREE)[c(5,3,4,1,2,6)])
+
 gss1 <-  gss1 %>% mutate(DEGREE = replace(DEGREE, DEGREE=="NA", NA))
 
 ggplot(data=gss1%>%filter(!is.na(DEGREE)),aes(x=AGE_num))+geom_histogram(stat="count")+facet_wrap(~DEGREE,ncol = 1)+theme(axis.text.x = element_text(angle = 90, hjust = 1))
@@ -52,9 +54,12 @@ gss1 %>% group_by(SEX) %>% filter(!is.na(AGEKDBRN)) %>% summarize(avg=mean(AGEKD
 
 avgs <- gss1 %>% group_by(YEAR,SEX,DEGREE) %>% filter(!is.na(AGEKDBRN)) %>% summarize(avg=mean(AGEKDBRN),sd=sd(AGEKDBRN),obs=n()) %>% filter(obs>=30)
 
-ggplot(data=avgs, aes(x=avg))+geom_histogram() + facet_wrap(~SEX)
-ggplot(data=avgs, aes(x=avg))+geom_histogram() + facet_wrap(SEX~DEGREE)
+ggplot(data=avgs, aes(x=avg))+geom_histogram(bins=15) + facet_wrap(~SEX)
+ggplot(data=avgs, aes(x=avg))+geom_histogram(bins=15) + facet_wrap(SEX~DEGREE)
 
+ggplot(data=avgs, aes(x=avg))+geom_histogram(bins=15,aes(fill=factor(DEGREE ))) + facet_wrap(SEX~YEAR)
+
+ggplot(data=avgs, aes(x=avg,fill=SEX))+geom_histogram(bins=15,aes(fill=factor(DEGREE))) + facet_wrap(~YEAR)
 
 
 
